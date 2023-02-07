d22337e8a9a82fa86ec2f1d29ccf60c503010bc8
seongunbae <122260955+seongunbae@users.noreply.github.com>
2023-02-07 11:18:51 +0900

    Add files via upload

---
+++ js/login.js
@@ -0,0 +1,58 @@
+
+  $(function(){//submenu
+  $('#submenu_wrap').hide();
+  $('.gnb li a').mouseenter(function(){
+    $('.submenu_wrap').stop().fadeIn();
+    $('.submenu_wrap').mouseleave(function(){
+      $('.submenu_wrap').stop().fadeOut();
+    })
+  });
+  });
+  
+  $(function(){
+  AOS.init();	
+  });
+  
+  $(document).ready(function(){
+  
+  $('.menu_btn>a').on('click', function(){
+      $('.menu_bg').show(); 
+      $('.sidebar_menu').show().animate({
+          left:0
+      });  
+  });
+  $('.close_btn>a').on('click', function(){
+      $('.menu_bg').hide(); 
+      $('.sidebar_menu').animate({
+          left: '-' + 100 + '%'
+                  },function(){
+                      $('.sidebar_menu').hide();          
+                  });  
+  });
+  
+  });
+  
+  const counter = ($counter, max) => {
+  let now = max;
+  const handle = setInterval(() => {
+    $counter.innerHTML = Math.ceil(max - now).toLocaleString();
+    if (now < 1) {
+      clearInterval(handle);
+    }
+    const step = now / 10;
+    now -= step;
+  }, 50);
+  }
+  window.onload = () => {
+  const $counter = document.querySelector(".count");
+  const max = 14125;
+  setTimeout(() => counter($counter, max), 500);
+  }
+  
+  $(function(){
+  $('.tes').mouseup(function(){
+    alert("준비중 입니다.");
+  });
+  });
+  
+  
